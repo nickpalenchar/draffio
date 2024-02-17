07434a9e10c78c038e82c4ab6e77d648eedc4293
Nick Palenchar <nickpal@nickpalenchar.com>
2024-02-17 13:12:37 -0500

    code layout

--- frontend/src/views/Draff.tsx
+++ frontend/src/views/Draff.tsx
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { Flex, Square, Box } from '@chakra-ui/react';
+import { Flex, Center, Box, Stack } from '@chakra-ui/react';
 import { EditorView, basicSetup } from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { gutter } from '@codemirror/gutter';
 
 export const Draff = () => {
   const editorRef = useRef<HTMLDivElement>(null);
@@ -12,6 +13,7 @@ export const Draff = () => {
       return;
     }
     const editor = new EditorView({
+      doc: '// Your code here',
       extensions: [basicSetup, javascript()],
       parent: editorRef.current,
     });
@@ -22,9 +24,23 @@ export const Draff = () => {
   }, []);
 
   return (
-    <Flex height="100vh" bg="red">
-      <Box minWidth="50%" bg="yellow.100" ref={editorRef}></Box>
-      <Box>textedit 2</Box>
-    </Flex>
+    <Stack h="100vh">
+      <Center h="5em">Hello</Center>
+      <Flex height="100%" bg="teal.800" margin="3em">
+        <Box minWidth="50%" bg="teal.50" ref={editorRef}>
+          <Box margin={'1.5em'} ref={editorRef} />
+        </Box>
+        <Box
+          paddingLeft="1.2em"
+          paddingTop="0.5em"
+          paddingRight="1.2em"
+          fontFamily={'monospace'}
+          fontWeight={'bold'}
+          color="white"
+        >
+          textedit 2
+        </Box>
+      </Flex>
+    </Stack>
   );
 };
