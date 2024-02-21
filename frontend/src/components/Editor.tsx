852a61eb38ebbaeac48cf6204259a2291e4b9a16
Nick Palenchar <nickpal@nickpalenchar.com>
2024-02-20 19:01:53 -0500

    disclosure

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useRef, useState } from 'react';
-import { Box, Button, Center, Spacer } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, Spacer, VStack } from '@chakra-ui/react';
 import { EditorView, basicSetup } from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 
@@ -37,8 +37,20 @@ export const Editor: FC<EditorParams> = ({ onExecute }) => {
   };
 
   return (
-    <>
-      <Box margin={'1.5em'} maxHeight="100%" ref={editorRef} /> <Spacer />
+    <VStack>
+      <Box
+        width="100%"
+        height="640px"
+        maxHeight="100%"
+        overflowY={'scroll'}
+        overflowX={'hidden'}
+      >
+        <Box background="yellow.50">
+          {' '}
+          <Box maxHeight="100%" width="100%" ref={editorRef} />
+        </Box>
+      </Box>
+      <Spacer />
       <Box height="100%">
         <Center minWidth="48px">
           <Button colorScheme="green" onClick={onRun}>
@@ -46,6 +58,6 @@ export const Editor: FC<EditorParams> = ({ onExecute }) => {
           </Button>
         </Center>
       </Box>
-    </>
+    </VStack>
   );
 };
