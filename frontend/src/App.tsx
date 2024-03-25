27ec4e5870aa36676312ebcb41ea0a2825b42936
Nick Palenchar <nickpal@nickpalenchar.com>
2024-03-25 16:04:38 -0400

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,5 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import { Draff } from './views/Draff';
-import { RandomDraff } from './views/RandomDraff';
 import { ChakraProvider, Container } from '@chakra-ui/react';
-
-// ROUTING
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: () => {
-      document.location.href = '/js/new';
-      return <div></div>;
-    },
-  },
-  {
-    path: '/js',
-    element: <RandomDraff />,
-  },
-  {
-    path: '/js/:id',
-    element: <Draff />,
-  },
-]);
 
 function App() {
   window.location.href = '/js/new';
